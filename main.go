c9614fe699b505014372b43e526c1aabd5b8822a
黄建华 <hjh@huangjianhuadeMacBook-Pro.local>
2020-09-03 01:08:46 +0800

    添加helloworld项目

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"hello/sayhi"
+)
+
+func main() {
+	fmt.Println("Hello world!")
+	sayhi.Sayhi()
+}
---
+++ sayhi/sayhi.go
@@ -0,0 +1,8 @@
+package sayhi
+
+import "fmt"
+
+//Sayhi 打招呼
+func Sayhi() {
+	fmt.Println("Hi~ everybody!")
+}
